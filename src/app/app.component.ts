9929f7a78e8601a495333c2a581440e0b170bc65
daniel.perez <daniel.perez@almundo.com>
2020-09-03 23:54:27 -0500

    feat: angular fire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ps-page';
+
+  ngOnInit() {
+    console.log(
+      `======================================
+Disfruta del pánico que te provoca
+tener la vida por delante.
+.
+.
+.
+Las experiencias de quienes nos precedieron,
+de nuestros “poetas muertos”,
+te ayudan a caminar por la vida
+La sociedad de hoy somos nosotros:
+Los “poetas vivos”.
+.
+.
+.
+No permitas que la vida te pase a ti sin que la vivas. 
+
+(Walt Whitman)
+
+ttps://www.youtube.com/watch?v=84hVojXBLY0
+
+======================================`
+    );
+  }
+
+  clearServiceWorker() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .getRegistrations()
+        .then(function(registrations) {
+          for (let registration of registrations) {
+            registration.unregister();
+          }
+        })
+        .catch(function(err) {
+          console.log('Service Worker registration failed: ', err);
+        });
+    }
+  }
 }
