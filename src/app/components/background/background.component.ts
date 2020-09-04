5d7a23c8be9b7c1c333d9a2009234091b74a3dfe
daniel.perez <daniel.perez@almundo.com>
2020-09-04 15:35:44 -0500

    fix: background in componente

---
+++ src/app/components/background/background.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ps-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.scss']
+})
+export class BackgroundComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
