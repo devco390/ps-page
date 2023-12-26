a255419aaa909e634d7bbbc83fc00728a9230bc8
daniel-p-meli <daniel.perez@mercadolibre.com.co>
2023-12-26 11:15:22 -0500

    change phone

--- src/app/data/data.js
+++ src/app/data/data.js
@@ -2,7 +2,7 @@ export const data = {
     phone: '3114386970',
     phoneShort: '311 438 69 70',
     phoneWhatsapp: '573114386970',
-    phoneMedellin: '311 4386970',
+    phoneMedellin: '3114386970',
     phoneShortMedellin: '311 438 69 70',
     phoneWhatsappMedellin: '573114386970',
     landlinePhone: '+57-0317037907'
